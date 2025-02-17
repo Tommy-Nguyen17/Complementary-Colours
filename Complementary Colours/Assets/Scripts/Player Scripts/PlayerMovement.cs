99c0cdbc2cc5ccf7cf15dbb9c2d68725c99142ac
TommyN17 <166442927+Tommy-Nguyen17@users.noreply.github.com>
2025-02-17 14:56:27 -0500

    Updates
    * Rain Generation now generates pixel art rain
    * Adds player that can move left and right

--- Complementary Colours/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Complementary Colours/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
+    private float speed = 2f;
     private bool isFacingRight = true;
 
     [SerializeField]
